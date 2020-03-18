65ac900c6d42d94597e0230fde8386779d9a7431
marco <amantil@protonmail.com>
2020-03-18 15:43:45 +0100

    aggiunta

--- src/ocp/chapter1/nestedClasses/Anonymous.java
+++ src/ocp/chapter1/nestedClasses/Anonymous.java
@@ -1,7 +1,9 @@
 package ocp.chapter1.nestedClasses;
 
 public class Anonymous {
-	//questa è una member inner class dichiarata come interfaccia
+	private int number;
+	
+	//questa è una member inner class dichiarata come interfaccia. E' anche una functional interface perchè abbiamo un singolo metodo.
 	interface Math {
 		public abstract void add ();
 	}
@@ -11,13 +13,17 @@ public class Anonymous {
 
 			@Override
 			public void add() {
-				// TODO Auto-generated method stub
-				System.out.println("somma di due numeri");
+				// TODO Auto-generated method stub 
+				System.out.println("somma di due numeri anonymous inner class " + number); //può accedere alle proprietà della outer class
 			}
 			
 		};
 		
+		//sfrutto math come lambda expression
+		Math function = () -> System.out.println("Somma di due numeri lambda expression");
+		
 		m.add();
+		function.add();
 	}
 	
 	public static void main (String[] args) {
