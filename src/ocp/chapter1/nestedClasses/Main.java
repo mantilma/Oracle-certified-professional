7a402a4dfc7e43b1b4ee1637afaef0dcce3a4918
marco <amantil@protonmail.com>
2022-03-24 20:03:43 +0100

    nested classes

---
+++ src/ocp/chapter1/nestedClasses/Main.java
@@ -0,0 +1,7 @@
+package ocp.chapter1.nestedClasses;
+
+public class Main {
+	public static void main (String[] args) {
+		
+	}
+}
--- src/ocp/chapter1/nestedClasses/MemberOuter.java
+++ src/ocp/chapter1/nestedClasses/MemberOuter.java
@@ -1,11 +1,13 @@
 package ocp.chapter1.nestedClasses;
 
-//this is an example of member inner class that is a class defined at the same level as instance variables
+//this is an example of member inner class that is a class defined at the same level as instance (member,properties) variables
 public class MemberOuter {
 	private String greeting = "Hi";
 	
-	private class Inner {
-		public int repeat = 3;
+	// dichiaro la member inner class e vediamo che è definita allo stesso livello dove dichiaro le proprietà costruttori e metodi
+	// posso utilizzare qualsiasi access modifiers (private default protected or public
+	private class Inner { 
+		private int repeat = 3;
 		public void go() {
 			for (int i = 0; i < repeat; i++)
 				System.out.println(greeting);
