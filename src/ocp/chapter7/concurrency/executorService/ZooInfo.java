73b6d88f9614e9b46a11af51de5144332a1b6e14
marco <amantil@protonmail.com>
2022-10-21 16:10:10 +0200

    WIP concurrency

--- src/ocp/chapter7/concurrency/executorService/ZooInfo.java
+++ src/ocp/chapter7/concurrency/executorService/ZooInfo.java
@@ -2,13 +2,16 @@ package ocp.chapter7.concurrency.executorService;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 //Approccio con executor service
 public class ZooInfo {
 	
 	public static void main(String[] args) {
 		//1 - Creo un pool con un solo thread. 
-		//In realtà creare un pool con un solo thread serve a poco ma è a scopo illustrativo.
+		//In realtà creare un pool con un solo thread serve a poco se voglio concurrency e parallelismo.
+		//diciamo che questo pool è utilizzato se io voglio avere la certezza che task1 viene processato prima
+		//del task 2, task 2 prima del task 3 ecc... ed anche a scopi didattici
 		ExecutorService service = null;
 		
 		try {
@@ -22,7 +25,10 @@ public class ZooInfo {
 										System.out.println(i);
 									}
 					);
-			service.execute(() -> System.out.println("Printing"));
+			// inviare task anziche con execute con il metodo submit che mi restituisce un future che contiene info sul task
+			Future<?> future = service.submit(() -> System.out.println("Printing"));
+			System.out.println(future.isDone());
+			
 			System.out.println("end");		
 		}
 		
@@ -33,10 +39,14 @@ public class ZooInfo {
 			//poiche pool in chiusura non posso più inviare task. Avrò una eccezione RejectionExecutionException
 			//service.execute(() -> System.out.println("Lancia eccezione"));
 			
-			//ritorna true poichè lo shutdown è iniziato
+			//ritorna true poichè lo shutdown è iniziato ma finche non completo tutti i task già in coda non è completato
 			System.out.println(service.isShutdown());
 			
-			//ritorna true se tutti i task sono completati
+			//ritorna true se tutti i task sono completati coda vuota quindi shutdown è terminato
+			System.out.println(service.isTerminated());
+			
+			
+			
 		}
 	}
 }
