f3cff41cb6f1f06448668d3297906ce4235a7b57
marco <amantil@protonmail.com>
2022-11-15 16:38:37 +0100

    WIP

--- src/ocp/chapter2/designPattern/ImmutableAnimal.java
+++ src/ocp/chapter2/designPattern/ImmutableAnimal.java
@@ -10,7 +10,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
 
-public final class ImmutableAnimal { // 1 prevent methods from being overriden
+public final class ImmutableAnimal { // 1 prevent methods from being overriden e pertanto non do la possibilità a nessuna sottoclasse di implementare metodi che vadano a modificare l immutabilità
 	
 	private final String species; // 2 mark all of the instance variables private and final
 	private final int age;
@@ -23,7 +23,8 @@ public final class ImmutableAnimal { // 1 prevent methods from being overriden
 		if(favoriteFoods == null) {
 			throw new RuntimeException("favoriteFoods is required");
 		}
-		this.favoriteFoods = new ArrayList<String>(favoriteFoods);
+		this.favoriteFoods = new ArrayList<String>(favoriteFoods); //qui fare attenzione perchè se io avessi scritto this.favoriteFoods = favoriteFooods io avrei fatto puntare allo stesso oggetto
+		//e avrei permesso al chiamante di avere accesso ad un oggetto che può modificare!!!
 	}
 
 	public String getSpecies() {
@@ -37,21 +38,23 @@ public final class ImmutableAnimal { // 1 prevent methods from being overriden
 	/*
 	 * Questo non è consentito perchè esternamente io posso chiamare i metodi di
 	 * List favoriteFoods e di fatto modificarla Nelle list infatti il discorso
-	 * dell'immutabilità si riferisce che non posso cambiare la referenza ma posso
-	 * chiamare i suoi metodi come ad esempio add!
+	 * dell'immutabilità creata con private final nella proprietà/attributo/campo e nel set all'interno delo costruttore mi permette
+	 * di non consintere il cambio della referenza dell'oggetto, ma se io permetto questo get qualcuno può sempre invocare dei metodi
+	 * come ad esempio add che cambino la lista!!!
 	 */
 
 	/*public List<String> getFavoriteFoods() {
 		return favoriteFoods;
 	}*/
 	
+	//posso però restituire un singolo elemento della lista
 	public String getFavoriteFood(int index) {
 		return this.favoriteFoods.get(index);
 	}
 	
 	/*
 	 * Se abbiamo bisogno di un metodo che modifichi una o più proprietà l'idea è quella di creare un nuovo
-	 * oggetto copiare gli elementi presenti più aggiungere la modifica necessaria. Ese:
+	 * oggetto ImmutableAnimal, copiare in esso gli elementi presenti più aggiungere la modifica necessaria, e restituirlo. Ese:
 	 */
 	public ImmutableAnimal addAge() {
 		List<String> favoriteFoods = new ArrayList<String>();
@@ -68,9 +71,6 @@ public final class ImmutableAnimal { // 1 prevent methods from being overriden
 		System.out.println(lion.getAge());
 		lion = lion.addAge();
 		System.out.println(lion.getAge());
-		
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		
 		
 	}
 	
